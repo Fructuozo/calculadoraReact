d8524c744158410590c45d65d5d32ef25ad3f6bd
Gabriel Fructuozo <104333184+Fructuozo@users.noreply.github.com>
2022-12-09 04:37:47 -0300

    Add files via upload

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #79451d;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 28px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #79451d;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        aling-items: flex-end;
+        padding: 0 20px;
+        font-size: 28px;
+        font-family: 'Roboto';
+        color: #fd8d32;
+
+    }
+`
